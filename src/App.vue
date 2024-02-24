21e3fa9b08ab7ce10d433cbd1228fbf0af979bee
Kaira <kairavictoria01@gmail.com>
2024-02-24 14:34:36 -0300

    feat: basic scenario with timer

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <main class="columns is-gapless is-multiline">
+    <div class="column is-one-quarter">
+      <BarraLateral />
+    </div>
+    <div class="column is-three-quarter">
+      <FormComponent />
+    </div>
+  </main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import BarraLateral from './components/BarraLateral.vue';
+import FormComponent from './components/Formulario.vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    BarraLateral,
+    FormComponent
+}
+});
+</script>
+
+<style></style>
